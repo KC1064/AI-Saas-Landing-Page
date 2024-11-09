d9f706fb08cb04082caa8d8b9591c6976c0eb2b4
KC1064 <kiron08072003@gmail.com>
2024-11-09 22:34:00 +0530

    Working on hero section

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,32 +1,25 @@
 import LogoIcon from "@/assets/logo.svg";
 import MenuIcon from "@/assets/icon-menu.svg"
+import Button from "@/utils/Button"
+
 export const Header = () => {
   return <header className="flex py-4 w-full border-b border-white/15 justify-between px-4 md:border-none md:justify-center">
     <div className="flex justify-between items-center w-full md:border md:border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
-    <div className="h-10 w-10 flex justify-center items-center border-white/20 rounded-lg border">
-      <LogoIcon className="h-8 w-8" />
-    </div>
-    <div className="md:block hidden items-center">
-      <nav className="flex items-center gap-8">
-        <a href="#" className="text-white/70 hover:text-white transition duration-200 ">Features</a>
-        <a href="#" className="text-white/70 hover:text-white transition duration-200 ">Developers</a>
-        <a href="#" className="text-white/70 hover:text-white transition duration-200 ">Pricing</a>
-        <a href="#" className="text-white/70 hover:text-white transition duration-200 ">Changelog</a>
-      </nav>
-    </div>
-    <div className="flex items-center gap-4">
-      <button className="relative px-3 py-2 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
-        <div className="absolute inset-0">
-          <div className="rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
-          <div className="rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(top,black,transparent)]"></div>
-          <div className="inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg"></div>
-        </div>
-        <span className="text-sm font-medium">
-          Join Waitlist
-        </span>
-      </button>
-      <MenuIcon className="md:hidden"/>
-    </div>
+      <div className="h-10 w-10 flex justify-center items-center border-white/20 rounded-lg border">
+        <LogoIcon className="h-8 w-8" />
+      </div>
+      <div className="md:block hidden items-center">
+        <nav className="flex items-center gap-8">
+          <a href="#" className="text-white/70 hover:text-white transition duration-200 ">Features</a>
+          <a href="#" className="text-white/70 hover:text-white transition duration-200 ">Developers</a>
+          <a href="#" className="text-white/70 hover:text-white transition duration-200 ">Pricing</a>
+          <a href="#" className="text-white/70 hover:text-white transition duration-200 ">Changelog</a>
+        </nav>
+      </div>
+      <div className="flex items-center gap-4">
+        <Button />
+        <MenuIcon className="md:hidden" />
+      </div>
     </div>
   </header>;
 };
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,3 +1,26 @@
+import Button from "@/utils/Button";
+import starbg from "@/assets/stars.png"
+
 export const Hero = () => {
-  return <section>Hero</section>;
+  return <section className="h-screen flex items-center"
+    style={{
+      backgroundImage: `url(${starbg.src})`
+    }}>
+    <div className="absolute h-64 w-64 bg-purple-500 rounded-full border-white/20 border top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,0.1),0_0_50px_rgb(140,69,255)]"></div>
+    <div className="absolute h-[380px] w-[380px] rounded-full border-white/20 border top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute h-3 w-3 left-0 bg-white/20 rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="absolute h-3 w-3 left-1/2 bg-white rounded-full top-0 -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="absolute h-6 w-6 left-full border border-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex items-center justify-center">
+      <div className="h-2 w-2 bg-white rounded-full"></div>
+      </div>
+    </div>
+    <div className="container relative">
+      <h1 className="text-8xl text-center tracking-tighter bg-white font-semibold bg-[radial-gradient(50%_100%_at_top_left,white,white,rgb(74,32,138,0.5))] bg-clip-text text-transparent">AI SEO</h1>
+      <p className="text-lg text-center text-white/70 mt-5 px-4">Elevate your site's visibility effortlessly with AI,
+        where smart technology meets user-friendly SEO tools.</p>
+      <div className="flex justify-center items-center mt-8">
+        <Button />
+      </div>
+    </div>
+  </section>;
 };
