be0a4bec01953ee7ee870cd9f4da3d6995e58694
KC1064 <kiron08072003@gmail.com>
2024-11-10 18:33:39 +0530

    Hero Section Complete

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,10 +1,10 @@
 import LogoIcon from "@/assets/logo.svg";
 import MenuIcon from "@/assets/icon-menu.svg"
-import Button from "@/utils/Button"
+import Button from "@/sections/Button"
 
 export const Header = () => {
-  return <header className="flex py-4 w-full border-b border-white/15 justify-between px-4 md:border-none md:justify-center">
-    <div className="flex justify-between items-center w-full md:border md:border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
+  return <header className="flex py-4 w-full border-b border-white/15 justify-between px-4 md:border-none md:justify-center sticky top-0 z-10 ">
+    <div className="flex justify-between items-center w-full md:border md:border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto backdrop-blur-xl">
       <div className="h-10 w-10 flex justify-center items-center border-white/20 rounded-lg border">
         <LogoIcon className="h-8 w-8" />
       </div>
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,22 +1,31 @@
-import Button from "@/utils/Button";
+import Button from "@/sections/Button";
 import starbg from "@/assets/stars.png"
 
 export const Hero = () => {
-  return <section className="h-screen flex items-center"
+  return <section className="h-[700px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)] "
     style={{
       backgroundImage: `url(${starbg.src})`
     }}>
-    <div className="absolute h-64 w-64 bg-purple-500 rounded-full border-white/20 border top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,0.1),0_0_50px_rgb(140,69,255)]"></div>
-    <div className="absolute h-[380px] w-[380px] rounded-full border-white/20 border top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(140,69,255,.5)_14%,rgb(14,0,36,.5)_78%,transparent)]"></div>
+    <div className="absolute h-64 w-64 md:h-96 md:w-96 bg-purple-500 rounded-full border-white/20 border top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,0.1),0_0_50px_rgb(140,69,255)]"></div>
+    <div className="absolute h-[380px] w-[380px] md:h-[580px] md:w-[580px] rounded-full border-white/20 border top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <div className="absolute h-3 w-3 left-0 bg-white/20 rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
       <div className="absolute h-3 w-3 left-1/2 bg-white rounded-full top-0 -translate-x-1/2 -translate-y-1/2"></div>
       <div className="absolute h-6 w-6 left-full border border-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex items-center justify-center">
-      <div className="h-2 w-2 bg-white rounded-full"></div>
+        <div className="h-2 w-2 bg-white rounded-full"></div>
+      </div>
+    </div>
+    <div className="absolute h-[450px] w-[450px] md:h-[650px] md:w-[650px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed"></div>
+    <div className="absolute h-[544px] w-[544px] md:h-[900px] md:w-[900px] rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute h-3 w-3 left-0 bg-white/20 rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="absolute h-3 w-3 left-full bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="absolute h-6 w-6 left-full border border-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex items-center justify-center">
+        <div className="h-2 w-2 bg-white rounded-full"></div>
       </div>
     </div>
     <div className="container relative">
-      <h1 className="text-8xl text-center tracking-tighter bg-white font-semibold bg-[radial-gradient(50%_100%_at_top_left,white,white,rgb(74,32,138,0.5))] bg-clip-text text-transparent">AI SEO</h1>
-      <p className="text-lg text-center text-white/70 mt-5 px-4">Elevate your site's visibility effortlessly with AI,
+      <h1 className="text-8xl md:text-[160px] md:leading-none text-center tracking-tighter bg-white font-semibold bg-[radial-gradient(50%_100%_at_top_left,white,white,rgb(74,32,138,0.5))] bg-clip-text text-transparent">AI SEO</h1>
+      <p className="text-lg md:text-xl max-w-xl mx-auto text-center text-white/70 mt-5 px-4">Elevate your site's visibility effortlessly with AI,
         where smart technology meets user-friendly SEO tools.</p>
       <div className="flex justify-center items-center mt-8">
         <Button />
