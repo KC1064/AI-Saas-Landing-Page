82db8c947632f40ebd5d9c949ae40869609acad7
KC1064 <kiron08072003@gmail.com>
2024-11-14 22:16:34 +0530

    Working on animations in CalltoAction section

--- sections/CallToAction.tsx
+++ sections/CallToAction.tsx
@@ -1,22 +1,71 @@
+"use client"
 import Button from "./Button";
 import starbg from "@/assets/stars.png"
 import gridLines from "@/assets/grid-lines.png"
+import { motion, useMotionTemplate, useMotionValue, useScroll, useTransform } from "motion/react"
+import { RefObject, useEffect, useRef } from "react";
+
+const useRelativeMousePosition = (to: RefObject<HTMLElement>) => {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const updateMousePosition = (event: MouseEvent) => {
+    if (!to.current) return;
+    const rect = to.current.getBoundingClientRect();
+    mouseX.set(event.x - rect.left);
+    mouseY.set(event.y - rect.top);
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', updateMousePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    }
+  })
+
+  return [mouseX, mouseY]
+}
 
 export const CallToAction = () => {
-  return <section className="py-20 md:py-24 px-2">
+  const sectionRef = useRef<HTMLElement>(null);
+  const borderDivRef = useRef<HTMLDivElement>(null)
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['start end', 'end start']
+  });
+
+  const [mouseX, mouseY] = useRelativeMousePosition(borderDivRef);
+
+  const maskImage = useMotionTemplate`radial-gradient(50% 50% at ${mouseX}px ${mouseY}px, black,transparent)`
+  const backgroundPositionY = useTransform(scrollYProgress, [0, 1], [-300, 300])
+  return <section ref={sectionRef} className="py-20 md:py-24 px-2">
     <div className="container">
-      <div className="border border-white/15 py-24 rounded-xl overflow-hidden relative" style={{
-        backgroundImage: `url(${starbg.src})`,
-        backgroundSize: "cover",
-      }}>
-        <div className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)] -z-10 " style={{
-          backgroundImage:`url(${gridLines.src})`
+      <motion.div
+        animate={{ backgroundPositionX: starbg.width }}
+        transition={{
+          duration: 20,
+          repeat: Infinity,
+          ease: "linear"
+        }}
+        className="border border-white/15 py-24 rounded-xl overflow-hidden relative group" style={{
+          backgroundImage: `url(${starbg.src})`,
+          backgroundPositionY
+        }}>
+        <div className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]  -z-10 group-hover:opacity-100 transition duration-700" style={{
+          backgroundImage: `url(${gridLines.src})`
         }}></div>
+        <motion.div className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay  -z-10 group-hover:opacity-0 transition duration-700" style={{
+          backgroundImage: `url(${gridLines.src})`,
+          maskImage
+        }}></motion.div>
         <h1 className="text-5xl md:text-6xl mx-auto tracking-tighter text-center font-medium">AI Driven SEO for everyone.</h1>
         <p className="text-center text-lg md:text-xl max-w-xs mx-auto text-white/70 px-4 mt-3 tracking-tight">Achieve clear, impactful results without the complexity.</p>
         <div className="flex justify-center mt-8"><Button />
         </div>
-      </div>
+      </motion.div>
     </div>
   </section>;
 };
+
+// 40
